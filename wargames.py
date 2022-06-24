1445bfcf1fd42d4b4467f63b0966e730f1a22fa0
Matt Morris <zekefoxdd@gmail.com>
2022-06-24 16:56:13 +0000

    Add files via upload

---
+++ wargames.py
@@ -0,0 +1,30 @@
+#This is my wargames project set to resemble the interaction between a character and a computer in the movie Wargames. I do not own 
+#or claim to have anything to do with this movie or any other parties involved with this movie, this is purely a project for fun and 
+#never meant to be shared with anyone besides myself and this software isn't meant to be shared.
+
+
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+print("\n")
+
+name = input("Login: ")
+#input("login: ")
+if  name == "joshua":
+	print("Hello proffessor, it's been a long time.\n")
+
+else:
+	print("Identification is not recognized by the system.")
+	print("--connection terminated--")
+	exit()
